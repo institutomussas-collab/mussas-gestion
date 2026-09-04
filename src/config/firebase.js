9934eef108243a4fbca48f36d3e34c751ec7c681
institutomussas-collab <institutomussas@gmail.com>
2026-09-04 17:03:47 -0300

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "mussas-gestion.firebaseapp.com",
+  projectId: "mussas-gestion",
+  storageBucket: "mussas-gestion.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
